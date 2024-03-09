938b2ebec47de92cb091646f43364003bbee764c
Igor Gazineo <gaumgazineo@hotmail.com>
2024-03-09 05:40:05 -0300

    day 38

--- 13-js-control-structures/conditional-code.js
+++ 13-js-control-structures/conditional-code.js
@@ -3,3 +3,15 @@ const myName = "Max";
 if (myName === "Max") {
   console.log("Hello!");
 }
+
+let isLoggedIn = true;
+
+if (!isLoggedIn) {
+  console.log("User is NOT logged in!");
+}
+
+const enteredUserName = "";
+
+if (enteredUserName) {
+  console.log("Input is valid!");
+}
--- 13-js-control-structures/demo.js
+++ 13-js-control-structures/demo.js
@@ -10,6 +10,19 @@ function updateRemainingCharacters(event) {
   const remainingCharacters = maxAllowedChars - enteredTextLength;
 
   remainingCharsElement.textContent = remainingCharacters;
+
+  if (remainingCharacters === 0) {
+    productNameInputElement.classList.add("error");
+    remainingCharsElement.classList.add("error");
+  } else if (remainingCharacters <= 10) {
+    productNameInputElement.classList.remove("error");
+    remainingCharsElement.classList.remove("error");
+    productNameInputElement.classList.add("warning");
+    remainingCharsElement.classList.add("warning");
+  } else {
+    productNameInputElement.classList.remove("warning");
+    remainingCharsElement.classList.remove("warning");
+  }
 }
 
 productNameInputElement.addEventListener("input", updateRemainingCharacters);
